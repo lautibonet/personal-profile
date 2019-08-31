756dd644f9985fc7fcf9c6a41259e14d85e05ca8
unknown <lautibonet@gmail.com>
2019-08-31 14:48:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+var _containers;
+var _currentPage;
+var _animating;
+
+$(document).ready(function () {
+    _animating = false;
+    _currentPage = 0;
+    _containers = $('.container');
+
+    $(window).bind('mousewheel', function (e) {
+        if (e.originalEvent.wheelDelta / 120 > 0) {
+            console.log('scrolling up !');
+            scrollUp();
+        }
+        else {
+            console.log('scrolling down !');
+            scrollDown();
+        }
+    });
+
+    $('span.letter').hover(function () {
+        $(this).addClass('rubberBand').delay(1000).queue(function (next) {
+            $(this).removeClass('rubberBand');
+            next();
+        });
+    });
+
+    $('#home').click(function () {
+        if ($('#home-wrapper').hasClass('active')) return;
+
+        $('.container div.active').toggleClass('active');
+        $('div#home-wrapper').toggleClass('active');
+    });
+
+    $('#profile').click(function () {
+        if ($('#profile-wrapper').hasClass('active')) return;
+
+        $('.container div.active').toggleClass('active');
+        $('div#profile-wrapper').toggleClass('active');
+
+    });
+
+    $('#experience').click(function () {
+        if ($('#experience-wrapper').hasClass('active')) return;
+
+        $('.container div.active').toggleClass('active');
+        $('div#experience-wrapper').toggleClass('active');
+
+    });
+
+    $('#contact').click(function () {
+        if ($('#contact-wrapper').hasClass('active')) return;
+
+        $('.container div.active').toggleClass('active');
+        $('div#contact-wrapper').toggleClass('active');
+
+    });
+});
+
+function scrollDown() {
+    if (_currentPage == _containers.length - 1) return;
+    $(_containers[_currentPage]).css('height', 0);
+    _currentPage++;
+}
+
+function scrollUp() {
+    if (_currentPage == 0) return;
+    $(_containers[_currentPage - 1]).css('height', '100vh');
+    _currentPage--;
+}
